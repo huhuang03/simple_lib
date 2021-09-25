1a713534b4daea47fd5e1c9e22fa6153b8041a44
york <york@duomai.com>
2021-09-26 00:38:08 +0800

    Initial

---
+++ include/simple_lib.h
@@ -0,0 +1,10 @@
+//
+// Created by huhua on 2021/9/25.
+//
+
+#ifndef SIMPLE_LIB_SIMPLE_LIB_H
+#define SIMPLE_LIB_SIMPLE_LIB_H
+
+void hello_from_simple_lib();
+
+#endif //SIMPLE_LIB_SIMPLE_LIB_H
---
+++ src/simple_lib.cc
@@ -0,0 +1,11 @@
+//
+// Created by huhua on 2021/9/25.
+//
+
+
+#include <iostream>
+#include <simple_lib.h>
+
+void hello_from_simple_lib() {
+    std::cout << "hello_from_simple_lib called" << std::endl;
+}
